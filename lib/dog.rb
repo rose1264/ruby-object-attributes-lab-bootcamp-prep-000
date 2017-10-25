51d5757ea88ff56ca563d5f60e5e1b046cb118d5
Shumei Huang <rose1264@gmail.com>
2017-10-25 05:21:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,24 @@
 class Dog
-  def name(name)
+  def initializeName(name)
     @name = name
   end
+
+  def name
+    @name
+  end
+  
   def name=(new_name)
     @name = new_name
   end
-  def breed(breed)
+  
+  def initializeBreed(breed)
     @breed = breed
   end
+
+  def breed
+    @breed
+  end
+  
   def breed=(new_breed)
     @breed = new_breed
   end
